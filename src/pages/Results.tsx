119dd450d58b95a94b10b6ac3d25513feab04016
Teemu Kakko <sormiteippi@windowslive.com>
2023-06-21 16:01:16 +0300

    Initial commit

---
+++ src/pages/Results.tsx
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import type { FC } from 'react';
+
+type Props = {};
+
+const Results: FC<Props> = ({ data }) => {
+  console.log(data);
+
+  return <div>Toimii</div>;
+};
+
+export default Results;
