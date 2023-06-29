0c1c25cdc568e8b2de0784c51cdbc077d8d458d4
Teemu Kakko <sormiteippi@windowslive.com>
2023-06-29 19:36:24 +0300

    Update getTags.ts

--- src/lib/getTags.ts
+++ src/lib/getTags.ts
@@ -1,8 +1,12 @@
-const getTags = (tags: Tags) => {
+import tags from './tags';
+
+const getTags = (photoTags: Tags) => {
   const tagArray: string[] = [];
 
-  tags.tag.map((tag) => {
-    tagArray.push(tag._content);
+  photoTags.tag.map((tag) => {
+    if (tags.some((tagObj) => tagObj.value === tag._content)) {
+      tagArray.push(tag._content);
+    }
   });
 
   return tagArray;
