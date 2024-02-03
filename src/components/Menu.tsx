97fd32dc63c095660724a5bd7a397523472e4129
Teemu Kakko <sormiteippi@windowslive.com>
2024-02-03 11:55:50 +0200

    Film buttons inside details

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -18,14 +18,21 @@ const Menu: FC<Props> = ({ selected, handleClick, handleReset }) => {
        * Film buttons
        */}
       <div>
-        {tags[0]?.map((tag, i) => (
-          <MenuButton
-            key={`f-button_${i}`}
-            tag={tag}
-            isPressed={selected.includes(tag.value)}
-            handleClick={handleClick}
-          />
-        ))}
+        <details open>
+          <summary className='mx-auto my-1 max-w-xs cursor-pointer px-5 py-2.5 font-montserrat text-xs font-bold text-green-900 sm:text-sm md:text-base'>
+            Film
+          </summary>
+          <div>
+            {tags[0]?.map((tag, i) => (
+              <MenuButton
+                key={`f-button_${i}`}
+                tag={tag}
+                isPressed={selected.includes(tag.value)}
+                handleClick={handleClick}
+              />
+            ))}
+          </div>
+        </details>
       </div>
 
       {/**
@@ -54,7 +61,7 @@ const Menu: FC<Props> = ({ selected, handleClick, handleReset }) => {
        */}
       <div>
         <button
-          className='m-1 rounded-3xl bg-neutral-300 px-5 py-2.5 font-montserrat text-xs font-light text-green-900 hover:bg-neutral-400 sm:text-sm md:text-base'
+          className='mx-1 mb-1 mt-3 rounded-3xl bg-neutral-300 px-5 py-2.5 font-montserrat text-xs font-light text-green-900 hover:bg-neutral-400 sm:text-sm md:text-base'
           onClick={() => handleReset()}
         >
           Reset
