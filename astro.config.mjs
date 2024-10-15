9f26baaac7b4c4a0852a3fc53bb8aa3194d5e7cf
Teemu Kakko <teemu.kakko@yle.fi>
2024-10-15 15:02:41 +0300

    Add OAuth authentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,23 @@ import react from '@astrojs/react';
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   base: '/film',
+  vite: {
+    define: {
+      'import.meta.env.CONSUMER_KEY': JSON.stringify(process.env.CONSUMER_KEY),
+      'import.meta.env.CONSUMER_SECRET': JSON.stringify(process.env.CONSUMER_SECRET),
+      'import.meta.env.OAUTH_TOKEN': JSON.stringify(process.env.OAUTH_TOKEN),
+      'import.meta.env.OAUTH_TOKEN_SECRET': JSON.stringify(process.env.OAUTH_TOKEN_SECRET),
+      'import.meta.env.PHOTOSET_ID': JSON.stringify(process.env.PHOTOSET_ID),
+      'import.meta.env.USER_ID': JSON.stringify(process.env.USER_ID),
+    },
+  },
 });
